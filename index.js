8d874ba2ce322a658f3d0afb64b82143573ceef9
MuhammadUmair151601 <157686720+MuhammadUmair151601@users.noreply.github.com>
2025-08-12 09:10:55 +0500

    metro discount offer practice done

---
+++ index.js
@@ -0,0 +1,35 @@
+
+// function askdetails(){
+// let age = +prompt('Please enter your age:')
+// let gender = prompt('please enter your gender:')
+
+// document.querySelector('#btn').innerText=`age ${age}gender ${gender}`;
+// if (gender === 'Female')
+// {
+//   alert('You got 50% discount');
+// }
+// else if(age < 5 && gender=== 'Male' || gender === 'Female'){
+//   alert('you are completely free')
+// }
+// else if(age > 5 && age <=8 && gender=== 'Male')
+// {
+//   alert('you got half ticket')
+// }
+// else if(age > 8 && age <= 65 && gender === 'Male'){
+//   alert('please pay the ticket fee in full')
+// }
+// else if(age > 65 && gender === 'Male'){
+//   alert('you got free ticket')
+// }
+// else if(age < 5 && gender === 'Female') {
+//   alert ('you get full discount')
+// }
+// else if(gender === 'Female' && age > 5 && age <=8)
+// {
+//   alert('you got full discount')
+// }
+// else{
+//   alert('please insert the correct values:')
+// }
+// }
+
